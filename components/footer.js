029de553933d11526ae4fdb953cfca79fc47e2ef
PressJump <40802367+PressJump@users.noreply.github.com>
2022-10-25 12:27:06 +1100

    Update Footer Date

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => (
             <hr class="my-10 border-gray-200 dark:border-gray-700" />
 
             <div class="flex flex-col items-center sm:flex-row sm:justify-between">
-                <p class="text-sm text-gray-400">© Copyright 2021. All Rights Reserved.</p>
+                <p class="text-sm text-gray-400">© Copyright {new Date().getFullYear()}. All Rights Reserved.</p>
 
                 <div class="flex mt-3 -mx-2 sm:mt-0">
                     <a href="/privacy-policy" class="mx-2 text-sm text-gray-400 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300">
